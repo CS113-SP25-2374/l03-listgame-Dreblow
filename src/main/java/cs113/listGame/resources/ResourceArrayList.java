9bad61df3cd81ceec5036bf010aa6b25f93d21d9
Derek Dreblow <derek.dreblow@gmail.com>
2025-02-24 20:36:30 -0800

    Completed ArrayList

--- src/main/java/cs113/listGame/resources/ResourceArrayList.java
+++ src/main/java/cs113/listGame/resources/ResourceArrayList.java
@@ -13,23 +13,34 @@ public class ResourceArrayList implements ResourceList {
     @Override
     public void add(ResourceObject resource) {
         if (contains(resource)) { return; } // gotcha moment, don't add it twice!
-        
-
+        resourceObjects.add(resource);
     }
 
     @Override
     public void remove(ResourceObject resource) {
-
+        resourceObjects.remove(resource);
     }
 
     @Override
     public void truncate(ResourceObject resource) {
+        int indexOfResource = resourceObjects.indexOf(resource);
 
+        // If the element was found, remove all elements from that index onward
+        if (indexOfResource != -1) {
+            for (int i = resourceObjects.size() - 1; i >= indexOfResource; i--) {
+                resourceObjects.remove(i);
+            }
+        }
     }
 
     @Override
     public void follow(GameObject leader) {
-
+        for (int i = 0; i < resourceObjects.length(); i++) {
+            if (resourceObjects.get(i) != null) {
+                resourceObjects.get(i).moveTowards(leader.getEchoCenter());
+                leader = resourceObjects.get(i);
+            }
+        }
     }
 
     private boolean contains(ResourceObject resource) {
